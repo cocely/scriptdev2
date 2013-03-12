f74cab1c907323e854874ad9635660f51a5a6a61
ike3 <ike@email.org>
2013-03-12 06:38:04 +0400

    autogenerated files (to be used without CMake in windows)

---
+++ revision.h
@@ -0,0 +1,6 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define REVISION_ID "38d9ee65bab759637dfeb0e20a931e2723876e44"
+ #define REVISION_DATE "*"
+ #define REVISION_TIME "*"
+#endif // __REVISION_H__
